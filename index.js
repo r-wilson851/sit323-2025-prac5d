5a96743beafc18fecd92c9f0c36d35c6553339da
r-wilson851 <s225235298@deakin.edu.au>
2025-05-25 23:30:41 +1000

    added files

---
+++ index.js
@@ -0,0 +1,222 @@
+const express= require("express");
+const app= express();
+
+
+// 1. Added helper functions to handle exponents, modulo & root 
+// 2. Added routes for each of the 3 new calculator functions along with their respective error handling & calling of the helper functinons 
+
+
+
+// Functions to handle the various calculator functions (adding, subtracting, multiplying, dividing)
+const add = (n1,n2) => {
+    return n1+n2;
+}
+
+const subtract = (n1,n2) => {
+    return n1 - n2;
+}
+
+const multiply = (n1,n2) => {
+    return n1 * n2;
+}
+
+const divide = (n1,n2) => {
+    return n1 / n2;
+}
+
+const exponent = (n1,n2) => {
+    return n1 ** n2;
+}
+
+const mod = (n1,n2) => {
+    return n1 % n2;
+}
+
+const root = (n1,n2) => {
+    return Math.sqrt(n1);
+}
+
+
+
+
+
+// Routes that take input parameters in request and perform calculation on them 
+app.get("/add", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = add(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+
+app.get("/subtract", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = subtract(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/multiply", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = multiply(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+
+app.get("/divide", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = divide(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/exponent", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = exponent(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/mod", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = mod(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/root", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = root(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port=3000;
+app.listen(port,()=> {
+    console.log("hello i'm listening to port " +port);
+})
